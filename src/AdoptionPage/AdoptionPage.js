7397dc4cb12fcc5a8afad05f8a0937a3c283475b
Justin Hager <coachjustinhager@gmail.com>
2021-02-11 14:43:03 -0800

    adds fetch for pet info

--- src/AdoptionPage/AdoptionPage.js
+++ src/AdoptionPage/AdoptionPage.js
@@ -5,11 +5,65 @@ export default class AdoptionPage extends Component {
     constructor(props){
         super(props);
         this.state = {
-           something: null,
-        };
+            petLoading: false,
+            cat: {
+                age: null,
+                breed: null,
+                description: null,
+                gender: null,
+                imageURL: null,
+                name: null,
+                story: null,
+            },
+            dog: {
+                age: null,
+                breed: null,
+                description: null,
+                gender: null,
+                imageURL: null,
+                name: null,
+                story: null,
+            }
+        }
+           
     };
     
+
+    componentDidMount(){
+        this.fetchPets();
+    }
+
+    fetchPets(){
+        this.setState({
+            petLoading: true,
+        })
+        fetch('https://zenmnky-petful-server.herokuapp.com/api/pets', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+        })
+        .then(res => res.json())
+        .then(data => {
+            let {cat, dog } = data;
+            this.setState({
+                petLoading: false,
+                dog: dog,
+                cat: cat
+            })
+        })
+    }
+    
     render() {
+
+             
+            let dog = this.state.dog;
+            let cat = this.state.cat;
+            let dogPlaceholder = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fwww.clipartkey.com%2Fmpngs%2Fm%2F5-56136_grayscale-dog-clipart-dog-silhouette-clip-art.png';
+            let catPlaceholder = 'https://external-content.duckduckgo.com/iu/?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F6%2F60%2FCat_silhouette.svg%2F1920px-Cat_silhouette.svg.png';
+
+
+
         return (
             <div className='flex-container'>
                 <h1>Adoption Page</h1>
@@ -24,28 +78,34 @@ export default class AdoptionPage extends Component {
                 </section>
                 <section id='adoption-pet-queue'>
                     <div id='adoption-pet-queue_dog-container'>
-                        <img alt='dog' />
+                        <img
+                            src={dog.imageURL || dogPlaceholder} 
+                            alt={`meet the dog called ${dog.name}`} 
+                        />
                         <div id='dog-story'>
                             <ul>
-                                <li>Name: </li>
-                                <li>Gender: </li>
-                                <li>Age: </li>
-                                <li>Breed: </li>
-                                <li>Story: </li>
+                                <li>Name: {dog.name}</li>
+                                <li>Gender: {dog.gender} </li>
+                                <li>Age: {dog.age}</li>
+                                <li>Breed: {dog.breed}</li>
+                                <li>Story: {dog.story}</li>
                             </ul>
                         </div>
                     </div>
                 </section>
                 <section id='adoption-pet-queue'>
                     <div id='adoption-pet-queue_cat-container'>
-                        <img alt='cat' />
+                        <img
+                            src={cat.imageURL || catPlaceholder}  
+                            alt={`meet the cat called ${cat.name}`}  
+                        />
                         <div id='cat-story'>
                             <ul>
-                                <li>Name: </li>
-                                <li>Gender: </li>
-                                <li>Age: </li>
-                                <li>Breed: </li>
-                                <li>Story: </li>
+                                <li>Name: {cat.name}</li>
+                                <li>Gender: {cat.gender} </li>
+                                <li>Age: {cat.age}</li>
+                                <li>Breed: {cat.breed}</li>
+                                <li>Story: {cat.story}</li>
                             </ul>
                         </div>
                     </div>
