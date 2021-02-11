21bc481b646a6931bf80bbc04e98e742a6ce5558
Justin Hager <coachjustinhager@gmail.com>
2021-02-11 14:00:37 -0800

    adoption page skeleton

--- src/AdoptionPage/AdoptionPage.js
+++ src/AdoptionPage/AdoptionPage.js
@@ -11,9 +11,62 @@ export default class AdoptionPage extends Component {
     
     render() {
         return (
-            <div>
+            <div className='flex-container'>
                 <h1>Adoption Page</h1>
+                <section id='adoption-name-queue'>
+                    <h2>Adoption Queue</h2>
+                    <ol>
+                        <li>name</li>
+                        <li>name</li>                        
+                        <li>name</li>
+                        <li>name</li>
+                    </ol>
+                </section>
+                <section id='adoption-pet-queue'>
+                    <div id='adoption-pet-queue_dog-container'>
+                        <img alt='dog' />
+                        <div id='dog-story'>
+                            <ul>
+                                <li>Name: </li>
+                                <li>Gender: </li>
+                                <li>Age: </li>
+                                <li>Breed: </li>
+                                <li>Story: </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+                <section id='adoption-pet-queue'>
+                    <div id='adoption-pet-queue_cat-container'>
+                        <img alt='cat' />
+                        <div id='cat-story'>
+                            <ul>
+                                <li>Name: </li>
+                                <li>Gender: </li>
+                                <li>Age: </li>
+                                <li>Breed: </li>
+                                <li>Story: </li>
+                            </ul>
+                        </div>
+                    </div>
+                </section>
+
             </div>
         )
     }
 }
+
+
+/**
+ * thinking: 
+ * what happens when we're routed to the adoption page?
+ * send requests to the API 
+ * - we need the next pets up for adoption - GET /api/pets?
+ * - we need to see the list of names in queue - GET /api/people
+ * 
+ * UI to display the above
+ * UI to add name to list + API POST request to add name
+ */
+
+
+
--- src/LandingPage/LandingPage.js
+++ src/LandingPage/LandingPage.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+
 
 export default function LandingPage() {
     return (
         <div>
-            <h1>Landing Page</h1>
+            <h1>Welcome to FICO Pet Adoption Services</h1>
+            <div className='flex-container'>
+                <Link to='/adopt'>
+                <img 
+                    src='https://images.unsplash.com/photo-1601758177266-bc599de87707?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80' 
+                    alt='heart warming dogs cat and person' 
+                />
+                </Link>
+                
+                <article id='adoption-process'>
+                    <h3>Adoption Process</h3>
+                    <p>Sign up to join the queue and watch as pets are matched with their new owners</p>
+                    <p>When your name becomes first in line, it's your turn to pick. We'll show you a cat and a dog that you may choose from.</p>
+                    <button><Link to='adopt'>Join the queue and adopt a pet</Link></button>
+                </article>
+            </div>
         </div>
     )
 }
